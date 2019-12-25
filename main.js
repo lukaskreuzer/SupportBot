6932821259488b1ef5d4366706b5eb9b56f52045
lukaskreuzer <59117251+lukaskreuzer@users.noreply.github.com>
2019-12-25 16:19:55 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const bot = new Discord.Client();
 
 const config = require('./config.json');
 bot.login(process.env.token); // for heroku variable
-
+//moin
 bot.on('voiceStateUpdate', (oldMember, newMember) => {
   let newUserChannel = newMember.voiceChannel;
   let oldUserChannel = oldMember.voiceChannel;
 
-  if (!newMember.roles.exists("name", config.targetRole)) { // supporters doesn't trigger the messages
+  if (!newMember.roles.some(x => x.name === config.targetRole)) { // supporters doesn't trigger the messages
     let supporters = getMembersFromRole(config.targetRole);
     if (newUserChannel !== undefined && newUserChannel.name === config.targetChannel) {  // user joins a channel
       sendMessageToMembers(supporters, `Hey! Der User **${newMember.displayName}** wartet im Support Channel auf dich! Schau vorbei :smile:`);
@@ -24,7 +24,7 @@ bot.on('voiceStateUpdate', (oldMember, newMember) => {
   function sendMessageToMembers(members, message) {
     members.forEach(member => {
       bot.users.get(member.id).send(message);
-      console.log("message send to " + member.username);
+      log(`Message sent to ${member.username}`);
     });
   }
   
@@ -32,6 +32,11 @@ bot.on('voiceStateUpdate', (oldMember, newMember) => {
 
 bot.on('message', msg => {
   if (!msg.author.bot && msg.channel.type === 'dm') {
+    log(`${msg.author.username} sent a private message: ${msg.content}`);
     msg.reply(`Hey! Solltest du Hilfe auf dem **${config.serverName}** Server benötigen, dann komm doch in den ${config.supportWaitingRoom} Channel! Ein Supporter wird sich schnellstmöglich um deine Anliegen kümmern :smile: Desweiteren kannst du auch gerne einen Supporter auf unseren Minecraft Server anschreiben :smile:`);
   }
 });
+
+function log(msg) {
+  console.log("Support Bot: " + msg);
+}
